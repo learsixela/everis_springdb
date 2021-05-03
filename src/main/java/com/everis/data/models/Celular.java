359b377f417c7b7659a3cbc9b7c7fb1a5205ffa7
Israel Alexis Palma Quezada <ipalm001@gmail.com>
2021-05-03 12:40:08 -0400

    Crud Celular

--- src/main/java/com/everis/data/models/Celular.java
+++ src/main/java/com/everis/data/models/Celular.java
@@ -1,10 +1,17 @@
 package com.everis.data.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="celulares")
@@ -15,6 +22,14 @@ public class Celular {
 	private String numero;
 	private String marca;
 	
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+	
+	 
 	public Celular() {}
 	
 	public Celular(String numero, String marca) {
@@ -36,6 +51,15 @@ public class Celular {
 		this.marca = marca;
 	}
 	
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+
 	
 }
 
